116dbecf54ee283c2732872d9f4f08ee1d907919
Chafiki <adamchafiki3@gmail.com>
2025-02-22 20:25:57 +0100

    ⭐  navbar,home  section

--- client/src/components/shared/Navbar.tsx
+++ client/src/components/shared/Navbar.tsx
@@ -1,14 +1,43 @@
-import { Link } from "react-router-dom";
+import {
+  Flex,
+  Button,
+  Link,
+  Spacer,
+  Image,
+  Container,
+} from "@chakra-ui/react";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <header>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/job">Job</Link>
-      </ul>
-    </header>
+    <Container p={4} maxW="1000px">
+      <Flex align="center" maxW="1200px" mx="auto">
+        {/* Logo */}
+        <Image src="/logo.png" alt="Logo" boxSize="50px" />
+
+        {/* Navigation Links */}
+        <Flex ml={10} gap={5}>
+          <Link href="#" _hover={{ textDecoration: "none", color: "blue.400" }}>
+            Browse
+          </Link>
+          <Link href="#" _hover={{ textDecoration: "none", color: "blue.400" }}>
+            Jobs
+          </Link>
+        </Flex>
+
+        <Spacer />
+
+        {/* Buttons */}
+        <Flex gap={4}>
+          <Button colorScheme="blue" colorPalette={"blue"} variant="outline">
+            Login
+          </Button>
+          <Button colorScheme="blue" colorPalette={"blue"}>
+            Register
+          </Button>
+        </Flex>
+      </Flex>
+    </Container>
   );
-}
+};
 
 export default Navbar;
--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,5 +1,26 @@
+import { Box, Button, Image, Text, HStack, VStack } from "@chakra-ui/react";
+import search from "@/assets/search.svg";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Box px={6} py={10} maxW="1000px" mx="auto">
+      <HStack spacing={10} align="center" justify="space-between">
+        <VStack align="start" spacing={4} maxW="500px">
+          <Text fontSize="3xl" fontWeight="bold" color="gray.800">
+            Find Your Dream Job Today!
+          </Text>
+          <Text fontSize="md" color="gray.500">
+            Thousands of jobs from top companies. Apply now and take the next
+            step in your career!
+          </Text>
+          <Button colorPalette={"blue"} size="lg">
+            Browse Jobs
+          </Button>
+        </VStack>
+        <Image src={search} alt="Job Search" boxSize="350px" />
+      </HStack>
+    </Box>
+  );
 }
 
 export default Home;
